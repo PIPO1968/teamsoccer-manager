256993381ead86dce937cdb7807f86a6ba97838f
PIPO1968 <pipo1968@email.com>
2026-02-28 22:38:23 +0000

    fix: CORS headers global middleware

--- backend/index.js
+++ backend/index.js
@@ -667,3 +667,11 @@ app.options('*', cors({ origin: allowedOrigin, credentials: true }));
 app.get('/test-redeploy', (req, res) => {
     res.send('Test de redeploy');
 });
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', allowedOrigin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
